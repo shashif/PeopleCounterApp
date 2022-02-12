42e6623c055d89bdfc487545c128dc0afe4ba39a
shashif <shoriful.hasan@gmail.com>
2022-02-12 14:08:02 +0600

    Initial commit

---
+++ app/src/main/java/com/example/peoplecounterapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.peoplecounterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val btnClick = findViewById<Button>(R.id.button)
+        val showText= findViewById<TextView>(R.id.textView)
+        var count=0
+
+        btnClick.setOnClickListener {
+
+            showText.text="Count "+ count
+            count++
+        }
+    }
+}
